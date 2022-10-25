07f9cbfd0571a1379cac073994c848b91c013f67
Alexis <alexis.mallassagne@epfedu.fr>
2022-10-25 16:01:39 +0200

    Question 12

---
+++ TP2_Bieres_Mallassagne/src/tp2_bieres_mallassagne/BouteilleBiere.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_mallassagne;
+
+/**
+ *
+ * @author xelat
+ */
+
+public class BouteilleBiere {
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+}
+    public void Décapsuler () {
+            if (ouverte=false){
+                ouverte=true;
+            }
+            else{
+                System.out.println("erreur : bière déjà ouverte") ;
+            }
+    }
+
+}
--- TP2_Bieres_Mallassagne/src/tp2_bieres_mallassagne/TP2_Bieres_Mallassagne.java
+++ TP2_Bieres_Mallassagne/src/tp2_bieres_mallassagne/TP2_Bieres_Mallassagne.java
@@ -14,7 +14,25 @@ public class TP2_Bieres_Mallassagne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ; //Définir un constructeur écrase la visibilité du constructeur par défaut, et il faut donc soit redéfinir un autre constructeur sans paramètres, soit modifier la création des objets précédent
+        uneBiere.nom = "Cuvée des trolls";
+        uneBiere.degreAlcool = 7.0;
+        uneBiere.lireEtiquette();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        autreBiere.nom = "Leffe";
+        autreBiere.degreAlcool = 6.6;
+        autreBiere.brasserie = "Abbaye de Leffe";
+        autreBiere.lireEtiquette();
+        BouteilleBiere deuxBiere = new BouteilleBiere("Desperados",5.9,"Tequila flavour") ;
+        deuxBiere.lireEtiquette();
+
+        BouteilleBiere troisBiere = new BouteilleBiere("Pelford",5.8,"Mon frigo") ;
+        troisBiere.lireEtiquette();
+
+        BouteilleBiere quatreBiere = new BouteilleBiere("La Chouffe",8.0,"Nain sur un vélo") ;
+        quatreBiere.lireEtiquette();
+
+
     }
     
 }
