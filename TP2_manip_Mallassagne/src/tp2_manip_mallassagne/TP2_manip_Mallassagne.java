5b7a5d746bc8c07a7cc4111e51ef92fb0146334a
Alexis <alexis.mallassagne@epfedu.fr>
2022-10-28 15:09:43 +0200

    Création du projet, création des 2 classes et affichage des calories

---
+++ TP2_manip_Mallassagne/src/tp2_manip_mallassagne/TP2_manip_Mallassagne.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_mallassagne;
+
+/**
+ *
+ * @author xelat
+ */
+public class TP2_manip_Mallassagne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        //l'assiette 2 est la même tartiflette que l'assiette 3 il n'y a donc que 2 assiettes créées
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+    }
+    
+}
---
+++ TP2_manip_Mallassagne/src/tp2_manip_mallassagne/Tartiflette.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_mallassagne;
+
+/**
+ *
+ * @author xelat
+ */
+public class Tartiflette {
+    public Tartiflette(int cal){
+        nbCalories=cal;
+    }
+    int nbCalories;
+}
+    
