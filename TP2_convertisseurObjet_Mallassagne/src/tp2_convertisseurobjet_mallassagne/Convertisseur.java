fba80e4951b94547863e27a6eb40ea80f12d6420
Alexis <alexis.mallassagne@epfedu.fr>
2022-10-30 18:27:27 +0100

    Ajout des commentaires et des JavaDocs

--- TP2_convertisseurObjet_Mallassagne/src/tp2_convertisseurobjet_mallassagne/Convertisseur.java
+++ TP2_convertisseurObjet_Mallassagne/src/tp2_convertisseurobjet_mallassagne/Convertisseur.java
@@ -8,13 +8,20 @@ package tp2_convertisseurobjet_mallassagne;
  *
  * @author xelat
  */
-public class Convertisseur {
+public class Convertisseur { //Création de la classe Convertisseur
 
-    public Convertisseur() {
+    public Convertisseur() { //Remplissage de la classe Convertisseur avec 1 attribut
         nbConversion = 0;
     }
     int nbConversion;
 
+    /**
+     * Prend en entrée la valeur en degrée Celcius puis la convertit en Kelvin
+     * et la renvoie
+     *
+     * @param celcius
+     * @return la temperature en Kelvin
+     */
     public double CelciusVersKelvin(double celcius) {
 
         double Kelvin = celcius + 273.15;
@@ -24,6 +31,13 @@ public class Convertisseur {
 
     }
 
+    /**
+     * Prend en entrée la valeur en degrée Kelvin puis la convertit en Celcius
+     * et la renvoie
+     *
+     * @param kelvin
+     * @return la temperature en Celcius
+     */
     public double KelvinVersCelcius(double kelvin) {
 
         double celcius = kelvin - 273.15;
@@ -33,6 +47,13 @@ public class Convertisseur {
 
     }
 
+    /**
+     * Prend en entrée la valeur en degrée Farenheit puis la convertit en
+     * Celcius et la renvoie
+     *
+     * @param farenheit
+     * @return la temperature en Celcius
+     */
     public double FarenheitVersCelcius(double farenheit) {
 
         double celcius = (farenheit - 32) / 1.8;
@@ -42,6 +63,13 @@ public class Convertisseur {
 
     }
 
+    /**
+     * Prend en entrée la valeur en degrée Celcius puis la convertit en
+     * Farenheit et la renvoie
+     *
+     * @param celcius
+     * @return la temperature en Farenheit
+     */
     public double CelciusVersFarenheit(double celcius) {
 
         double farenheit = (celcius * 1.8) + 32;
@@ -51,6 +79,13 @@ public class Convertisseur {
 
     }
 
+    /**
+     * Prend en entrée la valeur en degrée Kelvin puis la convertit en Farenheit
+     * et la renvoie
+     *
+     * @param kelvin
+     * @return la temperature en Farenheit
+     */
     public double KelvinVersFarenheit(double kelvin) {
 
         double farenheit = CelciusVersFarenheit(KelvinVersCelcius(kelvin));
@@ -60,6 +95,13 @@ public class Convertisseur {
 
     }
 
+    /**
+     * Prend en entrée la valeur en degrée Farenheit puis la convertit en kelvin
+     * et la renvoie
+     *
+     * @param farenheit
+     * @return la temperature en Kelvin
+     */
     public double FarenheitVersKelvin(double farenheit) {
 
         double kelvin = CelciusVersKelvin(FarenheitVersCelcius(farenheit));
@@ -68,6 +110,11 @@ public class Convertisseur {
         return kelvin;
     }
 
+    /**
+     * La methode toString retourne la chaine de caracteres que l'on souhaite
+     * afficher quand l'objet est cité. C'est une methode qui s'appelle
+     * automatiquement quand est cité dans System.out.println()
+     */
     @Override
     public String toString() {
         return "nb de conversions " + nbConversion;
--- TP2_convertisseurObjet_Mallassagne/src/tp2_convertisseurobjet_mallassagne/TP2_convertisseurObjet_Mallassagne.java
+++ TP2_convertisseurObjet_Mallassagne/src/tp2_convertisseurobjet_mallassagne/TP2_convertisseurObjet_Mallassagne.java
@@ -19,45 +19,49 @@ public class TP2_convertisseurObjet_Mallassagne {
         Convertisseur Temperat = new Convertisseur();
         Convertisseur Temperat2 = new Convertisseur();
         double temp = 1.0;
-        Temperat.FarenheitVersKelvin(temp);
+        Temperat.FarenheitVersKelvin(temp); //création de 2 objets
         Temperat2.CelciusVersFarenheit(temp);
         //System.out.println(Temperat); affichage du nombre de conversions de 2 objets 
         //System.out.println(Temperat2);
         System.out.println("Quel conversion voulez-vous faire ? C vers F (1), ou F vers C (2), C vers K (3), K vers C (4), K vers F (5) ou F vers K (6)");
-        Scanner sc= new Scanner(System.in) ;
-        int a =sc.nextInt();
-        if (a==1){
+        Scanner sc = new Scanner(System.in); //l'utilisateur peux rentrer la valeur souhaiter
+        int a = sc.nextInt();
+        if (a == 1) {//plusieurs choix de conversion
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.CelciusVersFarenheit(temp);
-            System.out.println(converchoisi);
+            System.out.println(converchoisi);// affichage du nombre de conversions
         }
-        if (a==2){
+        if (a == 2) {
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.FarenheitVersCelcius(temp);
             System.out.println(converchoisi);
         }
-        if (a==3){
+        if (a == 3) {
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.CelciusVersKelvin(temp);
             System.out.println(converchoisi);
         }
-        if (a==4){
+        if (a == 4) {
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.KelvinVersCelcius(temp);
             System.out.println(converchoisi);
         }
-        if (a==5){
+        if (a == 5) {
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.KelvinVersFarenheit(temp);
             System.out.println(converchoisi);
         }
-        if (a==6){
+        if (a == 6) {
             Convertisseur converchoisi = new Convertisseur();
             converchoisi.FarenheitVersKelvin(temp);
             System.out.println(converchoisi);
         }
-        
-                
+        // Scanner cc = new Scanner(System.in); tentative d'afficher la conversion
+        // int TempAConvert = cc.nextInt();
+        // Convertisseur TempConvert = new Convertisseur();
+        // TempConvert.FarenheitVersKelvin(TempAConvert);
+        // System.out.println(TempConvert);
+
     }
 
 }
