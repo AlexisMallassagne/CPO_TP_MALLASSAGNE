a522bfe547c2508fae6cbaed7cee5a60dce28bbf
Alexis <alexis.mallassagne@epfedu.fr>
2022-11-07 11:39:09 +0100

    Création d'un nouveau projet graphique

---
+++ Premier_Int_Graphique_MALLASSAGNE/src/premier_int_graphique_mallassagne/Premier_Int_Graphique_MALLASSAGNE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package premier_int_graphique_mallassagne;
+
+/**
+ *
+ * @author xelat
+ */
+public class Premier_Int_Graphique_MALLASSAGNE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
