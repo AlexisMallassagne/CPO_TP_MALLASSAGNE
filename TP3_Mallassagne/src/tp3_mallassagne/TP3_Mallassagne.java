4e3d84adc52ed1cf4fb3b279358b86673526f813
Alexis <alexis.mallassagne@epfedu.fr>
2022-11-16 14:55:57 +0100

    Ajout des commentaires

--- TP3_Mallassagne/src/tp3_mallassagne/TP3_Mallassagne.java
+++ TP3_Mallassagne/src/tp3_mallassagne/TP3_Mallassagne.java
@@ -22,28 +22,28 @@ public class TP3_Mallassagne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Arme arme1 = new Arme("arme standard",25);
+        Arme arme1 = new Arme("arme standard",25);//création d'une arme
         arme1.setAttaque(20);
         arme1.setAttaque(-20);
         System.out.println(arme1);
         
-        Baton baton1 = new Baton("Charme", 5, 6);
+        Baton baton1 = new Baton("Charme", 5, 6);//création de baton et d'epee
         Baton baton2 = new Baton("Chêne", 4, 5);
         Epee epee1 = new Epee("Durandal",4,7);
         Epee epee2 = new Epee("Excalibur",7,5);
-        ArrayList<Arme> TabArme= new ArrayList<>();
-        TabArme.add(baton1);
+        ArrayList<Arme> TabArme= new ArrayList<>();//Création d'un tableau de place infini d'arme
+        TabArme.add(baton1);//ajout d'arme dans le tableau
         TabArme.add(baton2);
         TabArme.add(epee1);
         TabArme.add(epee2);
         System.out.println(TabArme.get(0));
-        System.out.println(TabArme.size());
-        Magicien magicien1 = new Magicien("Gandalf",65,true);
+        System.out.println(TabArme.size());//affichage de la taille du tableau
+        Magicien magicien1 = new Magicien("Gandalf",65,true);//création de magiciens et de guerrier
         Magicien magicien2 = new Magicien("Garcimore",44,false);
         Guerrier guerrier1 = new Guerrier( "Conan", 78, false);
         Guerrier guerrier2 = new Guerrier( "Lannister", 45, true);
-        ArrayList<Personnage> TabPerso = new ArrayList<>();
-        TabPerso.add(magicien1);
+        ArrayList<Personnage> TabPerso = new ArrayList<>();//Création de tableau de perso
+        TabPerso.add(magicien1);//ajout de personnage
         TabPerso.add(guerrier2);
         TabPerso.add(guerrier1);
         TabPerso.add(magicien2);
@@ -56,16 +56,7 @@ public class TP3_Mallassagne {
         System.out.println(TabPerso.get(3));
         System.out.println(TabPerso.size());
         
-        
-        Arme verif = TabArme.get(0);
-        if (verif instanceof Baton){
-            System.out.println(verif+" est un baton");
-        }
-        else {
-            System.out.println(verif+" n'est pas un baton");
-        }
-        
-        Magicien Harry = new Magicien("Harry",56,false);
+        Magicien Harry = new Magicien("Harry",56,false);//création d'un magicien =, d'un guerrier, de 3 epees et de 3 batons
         Guerrier Doigby = new Guerrier("Doigby",45,false);
         Baton Bauldeumorv = new Baton ("Bauldeumorv",42,7);
         Baton boudboi = new Baton ("Boudboi",2,70);
@@ -74,16 +65,20 @@ public class TP3_Mallassagne {
         Epee coupecoupe = new Epee ("coupecoupe",75,23);
         Epee T = new Epee ("T",1,758000);
         
-        Harry.AjoutArme(Bauldeumorv);
+        Harry.AjoutArme(Bauldeumorv);//ajout d'epee et de batons dans "l'inventaire" du magicien et du guerrier
         Harry.AjoutArme(boudboi);
         Harry.AjoutArme(houillehouille);
         Doigby.AjoutArme(branche);
         Doigby.AjoutArme(coupecoupe);
         Doigby.AjoutArme(T);
-        Doigby.MethodeArme(T);
-        Harry.MethodeArme(boudboi);
-        Harry.ArmePréféré();
+        Doigby.PriseArme(T);//mise en main d'une arme choisi
+        Harry.PriseArme(boudboi);
+        Harry.ArmePréféré();//afficher le nombre d'arme preferes
         Doigby.ArmePréféré();
+        
+        System.out.println(Harry);
+        System.out.println(Doigby);
+        
         
     
     }
